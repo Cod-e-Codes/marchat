456d452e02b4a2e090114994b2eb5af4c241b830
Cod-e-Codes <codylmarsengill@gmail.com>
2026-09-01 09:34:00 -0400

    chore(packaging): sync v1.3.6 release checksums

--- packaging/chocolatey/tools/chocolateyinstall.ps1
+++ packaging/chocolatey/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 $tag    = 'v1.3.6'
 $url    = "https://github.com/Cod-e-Codes/marchat/releases/download/$tag/marchat-$tag-windows-amd64.zip"
-$checksum = '0000000000000000000000000000000000000000000000000000000000000000'
+$checksum = 'b24a2cdabf99e1561f703adfd0af986e12a58ce0059951bc8d7d615d67727cd5'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
--- packaging/homebrew/marchat.rb
+++ packaging/homebrew/marchat.rb
@@ -7,22 +7,22 @@ class Marchat < Formula
   on_macos do
     on_arm do
       url "https://github.com/Cod-e-Codes/marchat/releases/download/v1.3.6/marchat-v1.3.6-darwin-arm64.zip"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "97b236852decd4e05621ca24c4a910b6c5047516976b015f5c5ab3475d6f7c35"
     end
     on_intel do
       url "https://github.com/Cod-e-Codes/marchat/releases/download/v1.3.6/marchat-v1.3.6-darwin-amd64.zip"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "d5fcca4324fc6863e5ac19b95a6ed0d67124c5b6807412608ab01fffaae0c504"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/Cod-e-Codes/marchat/releases/download/v1.3.6/marchat-v1.3.6-linux-arm64.zip"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "5b8a2ab693ade484704ddf8c45d47272928a4a551b341d122d71dbf7a2a5d7f3"
     end
     on_intel do
       url "https://github.com/Cod-e-Codes/marchat/releases/download/v1.3.6/marchat-v1.3.6-linux-amd64.zip"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "a7e014e8ef161cea306c2cd2b08ab1a0265c852d10db0f1753f33963dde18ddf"
     end
   end
 
